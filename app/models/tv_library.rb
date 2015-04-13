60ff1e34bb39b348212055c69048e79ff376bb47
Sagar Gokhale <sagar.gokhale@gmail.com>
2015-04-12 18:47:46 -0700

    tv library scan and tests

--- app/models/tv_library.rb
+++ app/models/tv_library.rb
@@ -1,7 +1,96 @@
 class TvLibrary < MediaLibrary
   has_many :tv_shows, dependent: :destroy
-  
-  def after_scan
 
+  def after_scan
+    media_files = self.media_files.where(status: MediaFile::STATUS_ENABLED).all
+    media_files_hash = {}
+    media_library_path = self.path
+    media_files.each do |media_file|
+      full_name = media_file.path[media_library_path.length..-(File.extname(media_file.path).length+1)]
+      media_files_hash[full_name] ||= {video: nil, subtitles: nil}
+      media_files_hash[full_name][:subtitles] = media_file if media_file.is_subtitles_file?
+      media_files_hash[full_name][:video] = media_file if media_file.is_video_file?
+    end
+
+    # Get all the current meta data
+    seasons_hash = {}
+    shows_hash = {}
+
+    shows_by_name = {}
+    
+    episodes_hash = {}
+    self.tv_shows.includes(:tv_seasons).each do |tv_show|
+      shows_by_name[tv_show.name]||={object: tv_show, seasons: {}}
+      tv_show.tv_seasons.each do |tv_season|
+        shows_by_name[tv_show.name][:seasons][tv_season.name]=tv_season
+        seasons_hash[tv_season.id] ||= {tv_show_id: tv_show.id, exists: false, object: tv_season}
+        shows_hash[tv_show.id] = {exists: false, object: tv_show}
+        tv_season.tv_episodes.each do |tv_episode|
+          episodes_hash[tv_episode.media_file_id] = tv_episode
+        end
+      end
+    end
+    
+    media_files_hash.each do |full_name, media_files|
+      next if media_files[:video].blank?
+
+      video_file = media_files[:video]
+      tv_episode = episodes_hash[video_file.id] || TvEpisode.new(media_file_id: video_file.id)
+      if media_files[:subtitles]
+        tv_episode.subtitles_file_id = media_files[:subtitles].id
+      elsif tv_episode.subtitles_file_id
+        tv_episode.subtitles_file_id = nil
+      end
+      tv_episode.name = video_file.name if tv_episode.name.blank?
+
+      if tv_episode.tv_season_id
+        seasons_hash[tv_episode.tv_season_id][:exists] = true
+        shows_hash[seasons_hash[tv_episode.tv_season_id][:tv_show_id]][:exists] = true
+      else
+        dirname = File.split(full_name).first
+        dir_split = File.split(dirname)
+        name1 = dir_split.last
+        name2 = File.split(dir_split.first).last
+        if(name1==name2 || name2=="/")
+          show = name1
+          season = "default"
+        else
+          season = name1
+          show = name2
+        end
+
+        if shows_by_name[show]
+          tv_show = shows_by_name[show][:object]
+        else
+          tv_show = self.tv_shows.create(name: show)
+          shows_by_name[show] = {object: tv_show, seasons: {}}
+        end
+        if shows_by_name[show][:seasons][season]
+          tv_season = shows_by_name[show][:seasons][season]
+        else
+          tv_season = tv_show.tv_seasons.create(name: season)
+          shows_by_name[show][:seasons][season] = tv_season
+        end
+
+        tv_episode.tv_season_id = tv_season.id
+      end
+      
+      tv_episode.save if tv_episode.changed? || tv_episode.new_record?
+      
+      episodes_hash.delete(video_file.id)
+    end
+
+    episodes_hash.values.each do |tv_episode|
+      tv_episode.destroy
+    end
+    
+    seasons_hash.each do |tv_season_id, info|
+      info[:object].destroy if !info[:exists]
+    end
+
+    shows_hash.each do |tv_show_id, info|
+      info[:object].destroy if !info[:exists]
+    end
+    
   end
 end
--- test/models/tv_library_test.rb
+++ test/models/tv_library_test.rb
@@ -1,7 +1,150 @@
 require 'test_helper'
 
 class TvLibraryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    FileUtils.rm_rf(Rails.root.join("test/library_fixtures/tv_library1/show3"))
+  end
+  
+  test "scan path no episodes" do
+    m = TvLibrary.new(name: "Library 2", path: Rails.root.join("test/library_fixtures/library2"))
+    assert m.save!
+    assert_equal 2, m.media_files.where(status: MediaFile::STATUS_ENABLED).count
+    
+    m.media_files.all.each do |media_file|
+      assert media_file.valid?
+    end
+    assert m.scanned_at
+
+    assert_equal 0, m.tv_shows.count
+  end
+
+  test "scan path" do
+    tv_library = TvLibrary.first
+    tv_library.destroy
+
+    tv_library = TvLibrary.new(name: "TV Library 1", path: Rails.root.join("test/library_fixtures/tv_library1"))
+    tv_library.save!
+
+    assert_equal 2, tv_library.tv_shows.count
+    check_show1(tv_library)
+    check_show2(tv_library)
+
+    # Rescan
+    tv_library.scan
+    assert_equal 2, tv_library.tv_shows.count
+    check_show1(tv_library)
+    check_show2(tv_library)
+    
+  end
+
+  def check_show2(tv_library)
+    tv_show2 = tv_library.tv_shows.where(name: "show2").first
+    assert_equal 2, tv_show2.tv_seasons.count
+    season1 = tv_show2.tv_seasons.where(name: "season1").first
+    assert_equal 1, season1.tv_episodes.count
+    season2 = tv_show2.tv_seasons.where(name: "season2").first
+    assert_equal 1, season2.tv_episodes.count
+  end
+  
+  def check_show1(tv_library)
+    tv_show1 = tv_library.tv_shows.where(name: "show1").first
+    assert_equal 1, tv_show1.tv_seasons.count
+    tv_season1_1 = tv_show1.tv_seasons.first
+    assert_equal "default", tv_season1_1.name
+    assert_equal 2, tv_season1_1.tv_episodes.count
+    episode1 = tv_season1_1.tv_episodes.where(name: "video1").first
+    assert episode1.media_file
+    assert episode1.subtitles_file
+    episode2 = tv_season1_1.tv_episodes.where(name: "video2").first
+    assert episode2.media_file
+    assert !episode2.subtitles_file
+    
+  end
+  
+  test "scan path add remove things" do
+    tv_library = TvLibrary.first
+    tv_library.destroy
+
+    tv_library = TvLibrary.new(name: "TV Library 1", path: Rails.root.join("test/library_fixtures/tv_library1"))
+    tv_library.save!
+    
+    # Add a show, one season
+    add_video("episode1", "show3", "season1")
+    add_video("episode2", "show3", "season1")
+
+    tv_library.scan
+    check_show1(tv_library)
+    check_show2(tv_library)
+    assert_equal 3, tv_library.tv_shows.count
+
+    show3 = tv_library.tv_shows.where(name: "show3").first
+    assert_equal 1, show3.tv_seasons.count
+    season1 = show3.tv_seasons.where(name: "season1").first
+    assert_equal 2, season1.tv_episodes.count
+    
+    # Add a season to an existing show
+    add_video("episode1", "show3", "season2")
+    add_video("episode2", "show3", "season2")
+    add_video("episode3", "show3", "season2")
+    
+    tv_library.scan
+
+    show3 = tv_library.tv_shows.where(name: "show3").first
+    assert_equal 2, show3.tv_seasons.count
+    season1 = show3.tv_seasons.where(name: "season1").first
+    assert_equal 2, season1.tv_episodes.count
+
+    season2 = show3.tv_seasons.where(name: "season2").first
+    assert_equal 3, season2.tv_episodes.count
+    
+    # Add an episode to an existing season
+    add_video("episode4", "show3", "season2")
+
+    tv_library.scan
+
+    show3 = tv_library.tv_shows.where(name: "show3").first
+    season2 = show3.tv_seasons.where(name: "season2").first
+    assert_equal 4, season2.tv_episodes.count
+    
+    # remove an episode
+    remove_video("episode4", "show3", "season2")
+
+    tv_library.scan
+
+    show3 = tv_library.tv_shows.where(name: "show3").first
+    season2 = show3.tv_seasons.where(name: "season2").first
+    assert_equal 3, season2.tv_episodes.count
+
+    # remove a season
+    remove_video("episode1", "show3", "season2")
+    remove_video("episode2", "show3", "season2")
+    remove_video("episode3", "show3", "season2")
+
+    tv_library.scan
+
+    show3 = tv_library.tv_shows.where(name: "show3").first
+    assert_equal 1, show3.tv_seasons.count
+    assert !show3.tv_seasons.where(name: "season2").first
+    
+    # remove all seasons
+    remove_video("episode1", "show3", "season1")
+    remove_video("episode2", "show3", "season1")
+
+    tv_library.scan
+    assert_equal 2, tv_library.tv_shows.count
+    check_show1(tv_library)
+    check_show2(tv_library)
+  end
+
+  def remove_video(name, show, season)
+    FileUtils.rm_f(Rails.root.join("test/library_fixtures/tv_library1", show, season, "#{name}.mp4"))
+  end
+  
+  def add_video(name, show, season)
+    season_path = Rails.root.join("test/library_fixtures/tv_library1", show, season)
+    FileUtils.mkdir_p(season_path)
+    f = File.open(File.join(season_path, "#{name}.mp4"), "w")
+    f << "#{show} - #{season} - #{name}\n"
+    f.close
+  end
 end
